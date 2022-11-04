b4c1be35a444c506bf38d22b5b6debb3eee9da63
akshatmaheshwari1 <m253918@usna.edu>
2022-11-04 09:46:39 -0400

    Water Background

--- game.py
+++ game.py
@@ -1,19 +1,22 @@
 import pygame
 import sys
 
+TILE_SIZE = 64
 pygame.init()
-screen = pygame.display.set_mode((400,400))
+screen = pygame.display.set_mode((10*TILE_SIZE,10*TILE_SIZE))
+watertile = pygame.image.load('Assets/water_tile.png')
+
+water_rect = watertile.get_rect()
+screen_rect = screen.get_rect()
+for x in range(screen_rect.height//water_rect.width):
+    for y in range(screen_rect.width//water_rect.height):
+        screen.blit(watertile, (x * water_rect.width, y * water_rect.height))
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             print("thanks for playing")
-            sys.exit
-        if event.type == pygame.KEYUP:
-            screen.fill((0,0,255))
-        if event.type == pygame.KEYDOWN:
-            screen.fill((255,255,0))
-
+            sys.exit()
     pygame.display.flip()
 
 
