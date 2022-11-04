ba379ced633cf7b9e3c4fd0a07b3084b318125ab
akshatmaheshwari1 <m253918@usna.edu>
2022-11-04 09:04:12 -0400

    Update game.py

--- game.py
+++ game.py
@@ -1,1 +1,19 @@
-print("Mighty MIDY!")
+import pygame
+import sys
+
+pygame.init()
+screen = pygame.display.set_mode((400,400))
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            print("thanks for playing")
+            sys.exit
+        if event.type == pygame.KEYUP:
+            screen.fill((0,0,255))
+        if event.type == pygame.KEYDOWN:
+            screen.fill((255,255,0))
+
+    pygame.display.flip()
+
+
